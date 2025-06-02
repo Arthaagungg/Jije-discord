43c8533aaf20ed52e126824d3b7eaee815b68487
Arthaagungg <61904892+Arthaagungg@users.noreply.github.com>
2025-06-02 10:19:39 +0700

    Update voiceStateUpdate.js

--- src/events/Client/voiceStateUpdate.js
+++ src/events/Client/voiceStateUpdate.js
@@ -30,7 +30,7 @@ module.exports = new Event({
         });
 
         // Tambahkan jeda alami dan pelafalan lebih baik
-        const ttsText = `Halo...,${user.username}!`;
+        const ttsText = `Halo..., ${newState.member.displayName}!`;
         const ttsUrl = googleTTS.getAudioUrl(ttsText, {
           lang: 'id',
           speed: 0.9, // Lebih lambat sedikit
