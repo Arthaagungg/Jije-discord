a18d5f23828b9e2433135bc7e75cd23cf18b9ca1
Arthaagungg <61904892+Arthaagungg@users.noreply.github.com>
2025-06-03 20:40:22 +0700

    Create modal-sosmed_modal.js

---
+++ src/modals/Utility/modal-sosmed_modal.js
@@ -0,0 +1,31 @@
+const { ModalSubmitInteraction } = require('discord.js');
+const DiscordBot = require('../../client/DiscordBot');
+const Modal = require('../../structure/Modal');
+const socialManager = require('../../utils/socialManager');
+
+/**
+ * @param {DiscordBot} client 
+ * @param {ModalSubmitInteraction} interaction 
+ */
+module.exports = new Modal({
+    customId: /^sosmed_modal_(instagram|tiktok|x)$/,
+
+    run: async (client, interaction) => {
+        const match = interaction.customId.match(/^sosmed_modal_(instagram|tiktok|x)$/);
+        const platform = match?.[1];
+
+        if (!platform) {
+            return await interaction.reply({ content: 'Platform tidak valid.', ephemeral: true });
+        }
+
+        const url = interaction.fields.getTextInputValue('social_url');
+
+        if (!url.startsWith('http')) {
+            return await interaction.reply({ content: 'Link harus dimulai dengan http atau https.', ephemeral: true });
+        }
+
+        // Tambahkan atau update sosial media
+        socialManager.addSocial(interaction.user.id, platform, url);
+
+        await interaction.reply({
+            content: `✅ ${platform.charAt(
