41ed958612b7b39a691b6fba990f3fddae9b4791
Arthaagungg <61904892+Arthaagungg@users.noreply.github.com>
2025-06-03 23:29:17 +0700

    Update sosmed-actions-menu.js

--- src/components/SelectMenu/sosmed-actions-menu.js
+++ src/components/SelectMenu/sosmed-actions-menu.js
@@ -1,16 +1,15 @@
-const { StringSelectMenuInteraction } = require("discord.js");
 const DiscordBot = require("../../client/DiscordBot");
 const Component = require("../../structure/Component");
 
 module.exports = new Component({
-  customId: "sosmed_action_menu:\\d+",
-  type: "select",
-
-  /**
-   * @param {DiscordBot} client 
-   * @param {StringSelectMenuInteraction} interaction 
-   */
-  run: async (client, interaction) => {
+    customId: "sosmed_action_menu:\\d+",
+    type: 'select',
+    /**
+     * 
+     * @param {DiscordBot} client 
+     * @param {import("discord.js").AnySelectMenuInteraction} interaction 
+     */
+    run: async (client, interaction) => {
     const value = interaction.values[0];
     await interaction.reply({
       content: `✅ Kamu memilih: ${value}`,
