0cdf443d7159f0ac877c526bdd5ce65d6477e16f
Arthaagungg <61904892+Arthaagungg@users.noreply.github.com>
2025-06-08 17:51:01 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,32 +1,32 @@
 const config = {
     database: {
-        path: './database.yml' // The database path.
+        path: './database.yml' // Lokasi file database yang digunakan bot.
     },
     development: {
-        enabled: false, // If true, the bot will register all application commands to a specific guild (not globally).
-        guildId: '1354589321511960747',
+        enabled: false, // Jika true, bot akan mendaftarkan semua perintah hanya ke server tertentu (bukan global).
+        guildId: '1381216724027834408' // ID server tujuan untuk mode pengembangan.
     },
     commands: {
-        prefix: 'jj', // For message commands, prefix is required. This can be changed by a database.
-        message_commands: true, // If true, the bot will allow users to use message (or prefix) commands.
+        prefix: '!', // Prefix untuk perintah berbasis pesan. Dapat diubah melalui database.
+        message_commands: true, // Jika true, bot akan mengizinkan perintah melalui pesan (prefix command).
         application_commands: {
-            chat_input: false, // If true, the bot will allow users to use chat input (or slash) commands.
-            user_context: false, // If true, the bot will allow users to use user context menu commands.
-            message_context: false // If true, the bot will allow users to use message context menu commands.
+            chat_input: false, // Jika true, bot akan mengizinkan perintah berbasis slash (/).
+            user_context: false, // Jika true, bot akan mengizinkan perintah dari menu konteks pengguna.
+            message_context: false // Jika true, bot akan mengizinkan perintah dari menu konteks pesan.
         }
     },
     users: {
-        ownerId: '771528467249365003', // The bot owner ID, which is you.
-        developers: ['771528467249365003'] // The bot developers, remember to include your account ID with the other account IDs.
+        ownerId: '771528467249365003', // ID pemilik bot (kamu).
+        developers: ['771528467249365003'] // Daftar ID developer bot, pastikan ID kamu termasuk di sini.
     },
-    messages: { // Messages configuration for application commands and message commands handler.
-        NOT_BOT_OWNER: 'You do not have the permission to run this command because you\'re not the owner of me!',
-        NOT_BOT_DEVELOPER: 'You do not have the permission to run this command because you\'re not a developer of me!',
-        NOT_GUILD_OWNER: 'You do not have the permission to run this command because you\re not the guild owner!',
-        CHANNEL_NOT_NSFW: 'You cannot run this command in a non-NSFW channel!',
-        MISSING_PERMISSIONS: 'You do not have the permission to run this command, missing permissions.',
-        COMPONENT_NOT_PUBLIC: 'You are not the author of this button!',
-        GUILD_COOLDOWN: 'You are currently in cooldown, you have the ability to re-use this command again in \`%cooldown%s\`.'
+    messages: { // Konfigurasi pesan balasan saat menjalankan perintah.
+        NOT_BOT_OWNER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan pemilikku!',
+        NOT_BOT_DEVELOPER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan developer-ku!',
+        NOT_GUILD_OWNER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan pemilik server!',
+        CHANNEL_NOT_NSFW: 'Kamu tidak bisa menjalankan perintah ini di channel non-NSFW!',
+        MISSING_PERMISSIONS: 'Kamu tidak memiliki izin untuk menjalankan perintah ini, ada permission yang kurang.',
+        COMPONENT_NOT_PUBLIC: 'Kamu bukan pembuat tombol ini!',
+        GUILD_COOLDOWN: 'Kamu sedang dalam masa cooldown, kamu bisa menggunakan perintah ini lagi dalam \`%cooldown%s\`.'
     }
 }
 
