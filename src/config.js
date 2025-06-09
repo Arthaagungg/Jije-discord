3e66f6ebd279c00991058c5fdc5ceb5761203613
Arthaagungg <61904892+Arthaagungg@users.noreply.github.com>
2025-06-09 17:20:50 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,11 @@
 const config = {
-  bots: [
-    {
-      name: "Bot Utama",
-      token: process.env.BOT1_TOKEN,
-      ownerId: '771528467249365003'
-    },
-    {
-      name: "Bot Kedua",
-      token: process.env.BOT2_TOKEN, // Kosongkan ini kalau belum punya customer
-      ownerId: '771528467249365003'
-    }
-  ],
-
   database: {
     path: './database.yml'
   },
-
   development: {
     enabled: false,
     guildId: '1381216724027834408'
   },
-
   commands: {
     prefix: '!',
     message_commands: true,
@@ -30,17 +15,39 @@ const config = {
       message_context: false
     }
   },
-
+  users: {
+    ownerId: '771528467249365003',
+    developers: ['771528467249365003']
+  },
   messages: {
-    NOT_BOT_OWNER: 'Kamu bukan pemilik bot!',
-    NOT_BOT_DEVELOPER: 'Kamu bukan developer!',
-    NOT_GUILD_OWNER: 'Kamu bukan pemilik server!',
-    CHANNEL_NOT_NSFW: 'Perintah ini hanya untuk channel NSFW!',
-    MISSING_PERMISSIONS: 'Permission kamu tidak cukup!',
-    COMPONENT_NOT_PUBLIC: 'Tombol ini bukan milikmu!',
-    NOT_DEVELOPER_CHANNEL: 'Perintah ini hanya untuk channel developer!',
-    GUILD_COOLDOWN: 'Tunggu %cooldown%s untuk menjalankan perintah ini lagi.'
-  }
+    NOT_BOT_OWNER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan pemilikku!',
+    NOT_BOT_DEVELOPER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan developer-ku!',
+    NOT_GUILD_OWNER: 'Kamu tidak memiliki izin untuk menjalankan perintah ini karena kamu bukan pemilik server!',
+    CHANNEL_NOT_NSFW: 'Kamu tidak bisa menjalankan perintah ini di channel non-NSFW!',
+    MISSING_PERMISSIONS: 'Kamu tidak memiliki izin untuk menjalankan perintah ini, ada permission yang kurang.',
+    COMPONENT_NOT_PUBLIC: 'Kamu bukan pembuat tombol ini!',
+    NOT_DEVELOPER_CHANNEL: 'Kamu tidak bisa menjalankan perintah ini di dalam channel ini, silahkan menjalankan di dalam channel maintenance bot.',
+    GUILD_COOLDOWN: 'Kamu sedang dalam masa cooldown, kamu bisa menggunakan perintah ini lagi dalam `%cooldown%s`.'
+  },
+  bots: [
+    {
+      name: 'Bot Utama',
+      token: process.env.BOT1_TOKEN,
+      status: [
+        { name: 'Bot Utama Aktif 🤖', type: 0 },
+        { name: 'Melayani Server 1', type: 0 }
+      ]
+    },
+    {
+      name: 'Bot Kedua',
+      token: process.env.BOT2_TOKEN,
+      status: [
+        { name: 'Bot Kedua Siaga 🛡️', type: 0 },
+        { name: 'Online 24/7', type: 0 }
+      ]
+    }
+    // Tambahkan bot lainnya di sini
+  ]
 };
 
 module.exports = config;
